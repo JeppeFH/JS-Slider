6a10498e203a2c9db2b093c3b09de62ca9871aaa
JeppeFH <jeppefinch@hotmail.dk>
2024-08-13 11:01:50 +0200

    update js + img

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
 /*JS-Slider*/
+
+/* Variabler */
+const SliderOutput = document.querySelector(".slides");
+const slides = document.querySelectorAll(".single-slide");
+
+let slidesLength = slides.length - 1; /*index'et på sidste billede i slideren*/
+
+//variabel der siger at man skal starte med det første billede //
+let currentImageIndex = 0;
+
+/* function der bestemmer hvilket billede der vises */
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove(".active");
+  });
+}
