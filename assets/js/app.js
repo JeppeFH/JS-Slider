37fbe50c46560352d47d09ada8f0f5624d8e01ce
JeppeFH <jeppefinch@hotmail.dk>
2024-08-13 11:30:46 +0200

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,30 @@
 const SliderOutput = document.querySelector(".slides");
 const slides = document.querySelectorAll(".single-slide");
 
-let slidesLength = slides.length - 1; /*index'et på sidste billede i slideren*/
+/*index'et på sidste billede i slideren -1 [0, 1, 2]*/
+let slidesLength = slides.length - 1;
 
 //variabel der siger at man skal starte med det første billede //
 let currentImageIndex = 0;
 
 /* function der bestemmer hvilket billede der vises */
-function setActiveSlide() {
+function setActiveSlide(index) {
   slides.forEach((slide) => {
     slide.classList.remove("active");
   });
 
-  slides[0].classList.add("active");
+  slides[index].classList.add("active");
 }
+
+function next() {
+  if (currentImageIndex >= slidesLength) {
+    currentImageIndex = 0;
+  } else {
+    currentImageIndex += 1;
+  }
+
+  setActiveSlide(currentImageIndex);
+}
+
+setInterval(next, 2000);
+setActiveSlide(0);
